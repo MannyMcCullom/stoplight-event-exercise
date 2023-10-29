0ed284433b8b4fde60f786b5669eb7a13b8ad549
MannyMcCullom <m.mccullom11@gmail.com>
2023-10-29 19:36:49 -0400

    First Commit

--- index.js
+++ index.js
@@ -2,4 +2,21 @@
   'use strict';
 
   // YOUR CODE HERE
+  const stopBtn = document.querySelector('#stopButton');
+  stopBtn.addEventListener('click', ()=>{
+    stopLight.classList.toggle('stop');
+  })
+  const slowBtn = document.querySelector('#slowButton');
+  slowBtn.addEventListener('click', ()=>{
+    slowLight.classList.toggle('slow');
+  })
+  const goBtn = document.querySelector('#goButton');
+  goBtn.addEventListener('click', ()=>{
+    goLight.classList.toggle('go');
+  })
+  
+
+  const stopLight = document.querySelector('#stopLight');
+  const slowLight = document.querySelector('#slowLight');
+  const goLight = document.querySelector('#goLight');
 })();
